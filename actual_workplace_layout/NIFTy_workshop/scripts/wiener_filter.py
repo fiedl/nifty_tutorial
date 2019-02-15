487edbedf57c7217fc45756d5acfe1cebc5b7880
Knollmueller, Jakob (kjako) <jakob@knollmueller.de>
2019-02-15 10:21:15 +0100

    naming consistency

--- actual_workplace_layout/NIFTy_workshop/scripts/wiener_filter.py
+++ actual_workplace_layout/NIFTy_workshop/scripts/wiener_filter.py
@@ -6,21 +6,21 @@ np.random.seed(42)
 
 # want to implement: m = Dj = (S^{-1} + R^T N^{-1} R)^{-1} R^T N^{-1} d
 
-space = ift.RGSpace(256)
+position_space = ift.RGSpace(256)
 
-R = ift.GeometryRemover(space)
+R = ift.GeometryRemover(position_space)
 data_space = R.target
 data = np.load('../data_1.npy')
 data = ift.from_global_data(data_space, data)
 
-truth = np.load('../signal_1.npy')
-truth = ift.from_global_data(space, truth)
-plot_WF('data', truth, data)
+ground_truth = np.load('../signal_1.npy')
+ground_truth = ift.from_global_data(position_space, ground_truth)
+plot_WF('data', ground_truth, data)
 
 N = ift.ScalingOperator(0.1, data_space)
 
-harmonic_space = space.get_default_codomain()
-HT = ift.HartleyOperator(harmonic_space, target=space)
+harmonic_space = position_space.get_default_codomain()
+HT = ift.HartleyOperator(harmonic_space, target=position_space)
 
 def prior_spectrum(k):
     return 1/(10.+k**2)
@@ -36,11 +36,11 @@ D = ift.InversionEnabler(D_inv.inverse, IC, approximation=S)
 
 m = D(j)
 
-plot_WF('result', truth, data, m)
+plot_WF('result', ground_truth, data, m)
 
 S = ift.SandwichOperator.make(HT.adjoint, S_h)
 D = ift.WienerFilterCurvature(R, N , S, IC, IC).inverse
 N_samples = 10
 samples = [D.draw_sample()+m for i in range(N_samples)]
 
-plot_WF('result',m,data,truth,samples)
+plot_WF('result', m, data, ground_truth, samples)
